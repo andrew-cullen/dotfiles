f8d558f8e4ca3836687a6a22906ba35639f42f96
andrew-cullen <andrew-cullen@users.noreply.github.com>
2025-02-10 11:31:15 +1100

    Restructure

---
+++ lazyvim/.config/nvim/init.lua
@@ -0,0 +1,15 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- At the top, to help with debugging
+vim.lsp.set_log_level("debug")
+
+local lspconfig = require("lspconfig")
+lspconfig.ruff_lsp.setup({
+  cmd = { "/home/andrew/.local/bin/ruff", "server" },
+  filetypes = { "python" },
+  on_attach = function(client, bufnr)
+    --print("Ruff LSP attached!")
+    vim.api.nvim_echo({ { string.format("Ruff LSP attached to buffer %d", bufnr), "Normal" } }, true, {})
+  end,
+})
